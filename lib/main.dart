64ca2b152cdd47c1bb000aa3b636e38c5c8a82c9
JasonYHZ <coderyu2018@gmail.com>
2024-12-03 18:06:52 +0800

    feat: add dependent

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:jh_debug/jh_debug.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 void main() {
   jhDebugMain(
