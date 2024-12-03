9bc4fc3e431998097caf69d6974ca6e3adaf1fe1
JasonYHZ <coderyu2018@gmail.com>
2024-12-03 17:56:22 +0800

    feat: add localization support

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:jh_debug/jh_debug.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 void main() {
+  jhDebugMain(
+      appChild: const MyApp(),
+      debugMode: DebugMode.inConsole,
+      errorCallback: (details) {});
+
   runApp(const MyApp());
 }
 
@@ -10,14 +19,22 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    WidgetsFlutterBinding.ensureInitialized();
+
+    return ScreenUtilInit(
+      designSize: const Size(750, 1334),
+      child: MaterialApp(
+        onGenerateTitle: (context) => AppLocalizations.of(context).title,
+        locale: const Locale('zh', 'CH'),
+        localizationsDelegates: AppLocalizations.localizationsDelegates,
+        supportedLocales: AppLocalizations.supportedLocales,
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const Home(),
+        debugShowCheckedModeBanner: false,
       ),
-      home: const Home(),
-      debugShowCheckedModeBanner: false,
     );
   }
 }
@@ -29,10 +46,10 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Index Page"),
+        title: Text(AppLocalizations.of(context).title),
       ),
-      body: const Center(
-        child: Text("Index Page"),
+      body: Center(
+        child: Text(AppLocalizations.of(context).helloWorld),
       ),
     );
   }
