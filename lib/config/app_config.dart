a1054bb41e773f27bed0484496f00f22d49e8916
JasonYHZ <coderyu2018@gmail.com>
2024-12-04 13:38:23 +0800

    feat: add config

---
+++ lib/config/app_config.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_clean_arch/config/app_env.dart';
+
+import '../routes/route_name.dart';
+
+class AppConfig {
+  /// 设计稿尺寸 宽750 高1334
+  static Size screenSize = const Size(750, 1334);
+
+  /// 是否开启dio接口详细信息输出，以及其它相关插件调试信息
+  static const bool debug = true;
+
+  /// 是否开启LogUtil类打印方法
+  static const bool printFlag = true;
+
+  /// 是否直接跳过闪屏页面
+  static const bool notSplash = false;
+
+  /// 是否跳过引导页面
+  static const bool isShowWelcome = false;
+
+  /// 闪屏后跳转的页面（方便调试），需notSplash参数为true才有效果
+  static String directPageName = RouteName.appMain;
+
+  /// 是否显示jh_debug浮动按钮
+  static const bool showJhDebugBtn = true;
+
+  /// 是否开启更新app
+  static const bool isUpdateApp = true;
+
+  /// dio请求前缀
+  static String host = appEnv.baseUrl;
+
+  /// 是否启用代理，启用代理后，反向代理IP及端口才能生效
+  static const bool usingProxy = false;
+
+  /// 反向代理的IP:端口
+  static const String proxyAddress = "";
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_clean_arch/config/app_config.dart';
+import 'package:flutter_clean_arch/config/common_config.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:jh_debug/jh_debug.dart';
@@ -18,12 +20,17 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    jhDebug.setGlobalKey = commonConfig.getGlobalKey;
     WidgetsFlutterBinding.ensureInitialized();
 
     return ScreenUtilInit(
-      designSize: const Size(750, 1334),
+      designSize: AppConfig.screenSize,
+      minTextAdapt: true,
+      splitScreenMode: true,
       child: MaterialApp(
         onGenerateTitle: (context) => AppLocalizations.of(context).title,
+        navigatorKey: jhDebug.getNavigatorKey,
+        showPerformanceOverlay: false,
         locale: const Locale('zh', 'CH'),
         localizationsDelegates: AppLocalizations.localizationsDelegates,
         supportedLocales: AppLocalizations.supportedLocales,
@@ -51,5 +58,14 @@ class Home extends StatelessWidget {
         child: Text(AppLocalizations.of(context).helloWorld),
       ),
     );
+  }
+}
+
+class PageInfo extends StatelessWidget {
+  const PageInfo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
   }
 }
